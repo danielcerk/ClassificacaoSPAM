b106f053f597dad97c0a6a6f2f5e6e53b40e5b85
devdavifalcao <davivfalcao@hotmail.com>
2025-11-24 13:14:15 -0300

    Atualizando o repositorio cm os testes

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, request, jsonify, send_from_directory
-import re
+from train import spam_detector
 
 app = Flask(__name__, static_folder="static", static_url_path="/static")
 
+@app.get("/")
+def home():
+    return {"message": "API online"}
+    
 @app.route("/")
 def index():
-
     return send_from_directory(app.static_folder, "index.html")
 
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    if not data or "text" not in data:
+        return jsonify({"error": "Campo 'text' é obrigatório."}), 400
+
+    text = data.get("text", "")
+
+    try:
+        result = spam_detector(text)
+        return jsonify(result), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
 if __name__ == "__main__":
-    
     app.run(debug=True)
