647da251d398243670ffef104c61c1bd1598c110
Daniel Gomes <danielcerqueira7747@gmail.com>
2025-11-24 22:58:03 -0300

    feat: criado rota de dashboard

--- app.py
+++ app.py
@@ -1,33 +1,59 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, render_template
 from train import spam_detector
+from stats import get_spam_stats 
+import os
 
-app = Flask(__name__, static_folder="static", static_url_path="/static")
+app = Flask(__name__)
 
-@app.get("/")
-def home():
-    return {"message": "API online"}
-    
-@app.route("/")
+@app.route("/home")
 def index():
-    return send_from_directory(app.static_folder, "index.html")
+
+    return render_template("index.html")
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
+
     data = request.get_json()
 
     if not data or "text" not in data:
+
         return jsonify({"error": "Campo 'text' é obrigatório."}), 400
 
-    text = data.get("text", "")
+    text = data["text"]
 
     try:
+
         result = spam_detector(text)
+
         return jsonify(result), 200
 
     except Exception as e:
+
         return jsonify({"error": str(e)}), 500
 
 
+@app.route("/dashboard")
+def dashboard():
+
+    try:
+
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        csv_path = os.path.join(BASE_DIR, "emails.csv")
+
+        if not os.path.exists(csv_path):
+
+            return render_template("dashboard.html", stats=None, error="Arquivo emails.csv não encontrado.")
+
+        stats = get_spam_stats(csv_path)
+
+        return render_template("dashboard.html", stats=stats)
+
+    except Exception as e:
+
+        return render_template("dashboard.html", stats=None, error=str(e))
+
+
 if __name__ == "__main__":
+
     app.run(debug=True)
